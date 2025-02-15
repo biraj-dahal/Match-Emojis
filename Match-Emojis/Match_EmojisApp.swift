de471e2aefb77232e2163a4e67746e0c00201a90
biraj-dahal <dahalbiraj10@gmail.com>
2025-02-15 10:59:17 -0500

    Initial Commit

---
+++ Match-Emojis/Match_EmojisApp.swift
@@ -0,0 +1,17 @@
+//
+//  Match_EmojisApp.swift
+//  Match-Emojis
+//
+//  Created by Biraj Dahal on 2/15/25.
+//
+
+import SwiftUI
+
+@main
+struct Match_EmojisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
